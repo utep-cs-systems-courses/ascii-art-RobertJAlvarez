55807a4c5d18e8edb7ae1a8dd16e054a2942d5e2
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-06-17 14:57:29 -0600

    Fix minor changes

--- draw_chars.c
+++ draw_chars.c
@@ -1,7 +1,6 @@
 #include "draw.h"		/* for font */
-#include "stdio.h"		/* for putchar */
+#include "stdio.h"	/* for putchar */
 
-<<<<<<< HEAD
 void print_char(char c, int nRows, int nCols)
 {
   c -= 0x20;  //Rescale input because we ignore the first 20 ASCII characters
@@ -22,27 +21,3 @@ void print_char(char c, int nRows, int nCols)
   }
 }
 
-/*
-=======
->>>>>>> 240304530ab95a5b51c42c5e0b3818b6d42a6994
-void print_char_11x16(char c)
-{
-  c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_11x16[c][row];
-    for (char col = 0; col < 16; col++) {
-<<<<<<< HEAD
-      unsigned short colMask = 1 << (15-col); //Mask to select bit associated with bit
-=======
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
->>>>>>> 240304530ab95a5b51c42c5e0b3818b6d42a6994
-      putchar( (rowBits & colMask) ? '*' : ' ');
-    }
-    putchar('\n');
-  }
-}
-<<<<<<< HEAD
-*/
-
-=======
->>>>>>> 240304530ab95a5b51c42c5e0b3818b6d42a6994
